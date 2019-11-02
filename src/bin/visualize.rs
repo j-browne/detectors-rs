2748a87f777d208a2f5153c50dc3be061cfe9c6a
Justin Browne <jebdude89@gmail.com>
2019-11-02 19:38:25 -0400

    Add grid size option

--- src/bin/visualize.rs
+++ src/bin/visualize.rs
@@ -9,6 +9,8 @@ use structopt::StructOpt;
     about = "A program to calculate detector properties."
 )]
 struct Opt {
+    #[structopt(short = "g", default_value = "10")]
+    grid: u32,
     #[structopt(name = "FILE", parse(from_os_str))]
     files: Vec<PathBuf>,
 }
@@ -24,17 +26,17 @@ fn main() -> Result<(), Error> {
     let detectors = config.simplify()?;
 
     for (_, det) in detectors {
-        let max_u = 10;
-        let max_v = 10;
+        let max_u = opt.grid + 1;
+        let max_v = opt.grid + 1;
 
         let surface = det.surface();
         let u_limits = surface.u_limits_val();
         let v_limits = surface.v_limits_val();
 
         for u_idx in 0..max_u {
-            let u = f64::from(u_idx) * (u_limits.1 - u_limits.0) / f64::from(max_u) + u_limits.0;
+            let u = f64::from(u_idx) * (u_limits.1 - u_limits.0) / f64::from(max_u - 1) + u_limits.0;
             for v_idx in 0..max_v {
-                let v = f64::from(v_idx) * (v_limits.1 - v_limits.0) / f64::from(max_v) + v_limits.0;
+                let v = f64::from(v_idx) * (v_limits.1 - v_limits.0) / f64::from(max_v - 1) + v_limits.0;
 
                 let p = surface.coords_local_to_world(Point2::new(u, v));
                 println!("{} {} {}", p[0], p[1], p[2]);
