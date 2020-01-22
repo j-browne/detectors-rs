00d676d297fdaca0118dd8c078f589ebc532c60c
Justin Browne <jebdude89@gmail.com>
2020-01-22 12:07:37 -0500

    Organize test configurations better

--- tests/configurations.rs
+++ tests/configurations.rs
@@ -4,34 +4,34 @@ use std::io::Cursor;
 #[test]
 fn empty_object() {
     let mut config = Config::new();
-    let cursor = Cursor::new(include_str!("configurations/empty_object.json"));
+    let cursor = Cursor::new(include_str!("configurations/pass/empty_object.json"));
     assert!(config.add_from_reader(cursor).is_ok());
 }
 
 #[test]
 fn empty_fields() {
     let mut config = Config::new();
-    let cursor = Cursor::new(include_str!("configurations/empty_fields.json"));
+    let cursor = Cursor::new(include_str!("configurations/pass/empty_fields.json"));
     assert!(config.add_from_reader(cursor).is_ok());
 }
 
 #[test]
 fn end_cap() {
     let mut config = Config::new();
-    let cursor = Cursor::new(include_str!("configurations/end_cap.json"));
+    let cursor = Cursor::new(include_str!("configurations/pass/end_cap.json"));
     assert!(config.add_from_reader(cursor).is_ok());
 }
 
 #[test]
 fn empty() {
     let mut config = Config::new();
-    let cursor = Cursor::new(include_str!("configurations/empty.json"));
+    let cursor = Cursor::new(include_str!("configurations/fail/empty.json"));
     assert!(config.add_from_reader(cursor).is_err());
 }
 
 #[test]
 fn invalid_field() {
     let mut config = Config::new();
-    let cursor = Cursor::new(include_str!("configurations/invalid_field.json"));
+    let cursor = Cursor::new(include_str!("configurations/fail/invalid_field.json"));
     assert!(config.add_from_reader(cursor).is_err());
 }
